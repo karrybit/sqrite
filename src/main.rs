a4771f4575ab6039b97faf84dfa30b6ae1f2d136
makanai5610 <takumi.k.5610@gmail.com>
2020-06-06 09:37:46 +0900

    add repl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        print_prompt();
+        let mut command = String::new();
+        std::io::stdin().read_line(&mut command).unwrap_or_else(|e| panic!("{}", e));
+        command = command.trim().to_string();
+        if command == ".exit" {
+            break;
+        } else {
+            println!("Unrecognized command '{}'.", command);
+        }
+    }
+}
+
+fn print_prompt() {
+    print!("db > ");
+    std::io::stdout().flush().unwrap();
 }
